1410ff2ff8e89c4efc32824086b0989e25808f2c
Fernando Serrano <fernandojsg@gmail.com>
2020-05-29 23:09:04 +0200

    More benchmarks

--- benchmarks/components.bench.js
+++ benchmarks/components.bench.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { World } from "../src/World.js";
 import { Component3 } from "./helpers/components.js";
 
@@ -9,12 +10,24 @@ export function init(benchmarks) {
       prepare: ctx => {
         ctx.world = new World();
         ctx.world.registerComponent(Component3);
+=======
+import { World } from "../src";
+import { Component3, Component3NoReset } from "./helpers/components.js";
+
+export function init(benchmarks) {
+  benchmarks
+    .add({
+      name: "Entity::addComponent(Component3NoReset)",
+      prepare: ctx => {
+        ctx.world = new World();
+>>>>>>> More benchmarks
         for (let i = 0; i < 100000; i++) {
           ctx.world.createEntity();
         }
       },
       execute: ctx => {
         for (let i = 0; i < 100000; i++) {
+<<<<<<< HEAD
           ctx.world.entityManager._entities[i].addComponent(Component3);
         }
       }
@@ -24,12 +37,24 @@ export function init(benchmarks) {
       prepare: ctx => {
         ctx.world = new World();
         ctx.world.registerComponent(Component3);
+=======
+          ctx.world.entityManager._entities[i].addComponent(Component3NoReset);
+        }
+      },
+      iterations: 10
+    })
+    .add({
+      name: "Entity::addComponent(Component3NoReset)",
+      prepare: ctx => {
+        ctx.world = new World();
+>>>>>>> More benchmarks
         for (let i = 0; i < 100000; i++) {
           ctx.world.createEntity();
         }
       },
       execute: ctx => {
         for (let i = 0; i < 100000; i++) {
+<<<<<<< HEAD
           ctx.world.entityManager._entities[i].addComponent(Component3);
         }
       }
@@ -41,11 +66,25 @@ export function init(benchmarks) {
         ctx.world.registerComponent(Component3);
         for (let i = 0; i < 100000; i++) {
           ctx.world.createEntity().addComponent(Component3);
+=======
+          ctx.world.entityManager._entities[i].addComponent(Component3NoReset);
+        }
+      },
+      iterations: 10
+    })
+    .add({
+      name: "Entity::removeComponent(Component3NoReset)",
+      prepare: ctx => {
+        ctx.world = new World();
+        for (let i = 0; i < 100000; i++) {
+          ctx.world.createEntity().addComponent(Component3NoReset);
+>>>>>>> More benchmarks
         }
       },
       execute: ctx => {
         for (let i = 0; i < 100000; i++) {
           ctx.world.entityManager._entities[i].removeComponent(
+<<<<<<< HEAD
             Component3
           );
         }
@@ -58,11 +97,26 @@ export function init(benchmarks) {
         ctx.world.registerComponent(Component3);
         for (let i = 0; i < 100000; i++) {
           ctx.world.createEntity().addComponent(Component3);
+=======
+            Component3NoReset
+          );
+        }
+      },
+      iterations: 10
+    })
+    .add({
+      name: "Entity::removeComponent(Component3NoReset)",
+      prepare: ctx => {
+        ctx.world = new World();
+        for (let i = 0; i < 100000; i++) {
+          ctx.world.createEntity().addComponent(Component3NoReset);
+>>>>>>> More benchmarks
         }
       },
       execute: ctx => {
         for (let i = 0; i < 100000; i++) {
           ctx.world.entityManager._entities[i].removeComponent(
+<<<<<<< HEAD
             Component3
           );
         }
@@ -75,11 +129,26 @@ export function init(benchmarks) {
         ctx.world.registerComponent(Component3);
         for (let i = 0; i < 100000; i++) {
           ctx.world.createEntity().addComponent(Component3);
+=======
+            Component3NoReset
+          );
+        }
+      },
+      iterations: 10
+    })
+    .add({
+      name: "Entity::removeComponent(Component3NoReset) sync",
+      prepare: ctx => {
+        ctx.world = new World();
+        for (let i = 0; i < 100000; i++) {
+          ctx.world.createEntity().addComponent(Component3NoReset);
+>>>>>>> More benchmarks
         }
       },
       execute: ctx => {
         for (let i = 0; i < 100000; i++) {
           ctx.world.entityManager._entities[i].removeComponent(
+<<<<<<< HEAD
             Component3,
             true
           );
@@ -93,16 +162,40 @@ export function init(benchmarks) {
         ctx.world.registerComponent(Component3);
         for (let i = 0; i < 100000; i++) {
           ctx.world.createEntity().addComponent(Component3);
+=======
+            Component3NoReset,
+            true
+          );
+        }
+      },
+      iterations: 10
+    })
+    .add({
+      name: "Entity::removeComponent(Component3NoReset) sync",
+      prepare: ctx => {
+        ctx.world = new World();
+        for (let i = 0; i < 100000; i++) {
+          ctx.world.createEntity().addComponent(Component3NoReset);
+>>>>>>> More benchmarks
         }
       },
       execute: ctx => {
         for (let i = 0; i < 100000; i++) {
           ctx.world.entityManager._entities[i].removeComponent(
+<<<<<<< HEAD
             Component3,
             true
           );
         }
       }
+=======
+            Component3NoReset,
+            true
+          );
+        }
+      },
+      iterations: 10
+>>>>>>> More benchmarks
     });
 
   /*
@@ -118,7 +211,12 @@ export function init(benchmarks) {
         for (let i = 0; i < 100000; i++) {
           ctx.world.entityManager._entities[i].addComponent(Component3);
         }
+<<<<<<< HEAD
       }
+=======
+      },
+      iterations: 10
+>>>>>>> More benchmarks
     });
     */
 }
